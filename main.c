c8802c63346f93dc610a3b921abdd941c8b70cb2
Vladislav1102 <darknoem921@gmail.com>
2024-10-04 08:50:57 +0300

    add_timeshtamp

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/time.h>
+#include <time.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 FILE *log_file;
 
@@ -21,7 +23,19 @@ void init_log_file() {
     }
 }
 
-void write_log(const char * format, ...) {
+void write_log(bool print_timeshtamp, const char * format, ...) {
+    char buffer_time[256] = {};
+
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    struct tm *tm_info = localtime(&tv.tv_sec);
+
+    strftime(buffer_time, sizeof(buffer_time), "%Y-%m-%d %H:%M:%S", tm_info);
+
+    if(print_timeshtamp) {
+        fprintf(log_file, "%s.%06ld\t", buffer_time, tm_info);
+    }
+
     va_list args;
     va_start(args, format);
     vfprintf(log_file, format, args);
@@ -41,10 +55,10 @@ int open_socket(const char * interface_name) {
 
     if (can_socket > 0) {
         printf("Socket opened\n");
-        write_log("Socket opened\n");
+        write_log(1, "Socket opened\n");
     } else {
         printf("Error opening socket\n");
-        write_log("Error opening socket\n");
+        write_log(1, "Error opening socket\n");
     }
 
     strcpy(can_interface.ifr_name, interface_name);
@@ -55,10 +69,10 @@ int open_socket(const char * interface_name) {
 
     if (bind(can_socket, (struct sockaddr *)&can_addres, sizeof(can_addres)) < 0) {
         printf("Error connecting socket\n");
-        write_log("Error connecting socket\n");
+        write_log(1, "Error connecting socket\n");
     } else {
         printf("Socket connected\n");
-        write_log("Socket connected\n");
+        write_log(1, "Socket connected\n");
     }
 
     return can_socket;
@@ -77,12 +91,12 @@ int main() {
     while(1) {
         int frame = read(can_socket, &can_frame, sizeof(can_frame));
         if(frame > 0) {
-            write_log("can_id[%X]\t", can_frame.can_id);
-            write_log("frame_len[%X]\t", can_frame.len);
+            write_log(1, "can_id[%X]\t", can_frame.can_id);
+            write_log(0, "frame_len[%X]\t", can_frame.len);
             for(int i = 0; i < can_frame.can_dlc; i++) {
-                write_log("%X ", can_frame.data[i]);   
+                write_log(0, "%X ", can_frame.data[i]);
             }
-            write_log("\n");
+            write_log(0, "\n");
         }
         usleep(30000);
     }
