909ef8c0e431122f456e84a36899eb0bccce3481
Vladislav1102 <darknoem921@gmail.com>
2024-10-14 21:12:06 +0300

    add CMake build

---
+++ build/CMakeFiles/CANanalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CANanalyzer"
+  "CANanalyzer.pdb"
+  "CMakeFiles/CANanalyzer.dir/src/checking_err.c.o"
+  "CMakeFiles/CANanalyzer.dir/src/checking_err.c.o.d"
+  "CMakeFiles/CANanalyzer.dir/src/main.c.o"
+  "CMakeFiles/CANanalyzer.dir/src/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CANanalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
